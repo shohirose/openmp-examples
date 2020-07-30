47449904fcdb62235aa2056a6d808992a55728ca
Sho Hirose <sho.hirose@gmail.com>
2020-07-30 16:01:01 +0900

    fix: Typos

--- src/example1.cpp
+++ src/example1.cpp
@@ -3,37 +3,37 @@
 
 void SingleForLoopWithMaxNumThreads(int numLoops);
 void SingleForLoopWithTwoThreads(int numLoops);
-void DoubleForLoopWithMaxNumThreads(int numOuterLoops, int numInnerLoops);
+void DoubleForLoopsWithMaxNumThreads(int numOuterLoops, int numInnerLoops);
 
 int main() {
   printf("Maximum number of threads = %d\n\n", omp_get_max_threads());
 
   SingleForLoopWithMaxNumThreads(10);
   SingleForLoopWithTwoThreads(10);
-  DoubleForLoopWithMaxNumThreads(10, 2);
+  DoubleForLoopsWithMaxNumThreads(10, 2);
 }
 
-void SingleForLoopWithMaxNumThreads(int numOuterLoops) {
+void SingleForLoopWithMaxNumThreads(int numLoops) {
   printf("[Single for-loop with %d threads]\n", omp_get_max_threads());
 
 #pragma omp parallel for
-  for (int i = 0; i < numOuterLoops; ++i) {
+  for (int i = 0; i < numLoops; ++i) {
     printf("thread = %d, loop counter = %d\n", omp_get_thread_num(), i);
   }
   printf("\n");
 }
 
-void SingleForLoopWithTwoThreads(int numOuterLoops) {
+void SingleForLoopWithTwoThreads(int numLoops) {
   printf("[Single for-loop with %d threads]\n", 2);
 
 #pragma omp parallel for num_threads(2)
-  for (int i = 0; i < numOuterLoops; ++i) {
+  for (int i = 0; i < numLoops; ++i) {
     printf("thread = %d, loop counter = %d\n", omp_get_thread_num(), i); 
   }
   printf("\n");
 }
 
-void DoubleForLoopWithMaxNumThreads(int numOuterLoops, int numInnerLoops) {
+void DoubleForLoopsWithMaxNumThreads(int numOuterLoops, int numInnerLoops) {
   printf("[Double for-loops with %d threads]\n", omp_get_max_threads());
 
 #pragma omp parallel for
