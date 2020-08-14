7f65a7003de7c88c259a690e753d24f322b790ca
Sho Hirose <sho.hirose@gmail.com>
2020-08-14 19:35:04 +0900

    style: Add comments

--- src/gather.cpp
+++ src/gather.cpp
@@ -1,6 +1,7 @@
 #include <omp.h>
 
 #include <iostream>
+#include <sstream>
 #include <vector>
 
 void gatherStdVector();
@@ -15,7 +16,6 @@ void gatherStdVector() {
 #pragma omp parallel
   {
     std::vector<int> slave;
-
 #pragma omp for
     for (int i = 0; i < n; ++i) {
       slave.push_back(omp_get_thread_num());
@@ -23,19 +23,21 @@ void gatherStdVector() {
 
 #pragma omp critical
     {
-      std::cout << '[' << omp_get_thread_num() << ']';
-      for (auto value : slave) {
-        std::cout << ' ' << value;
-      }
-      std::cout << '\n';
-      master.insert(master.end(), std::make_move_iterator(slave.begin()),
+      // Prints slaves
+      std::stringstream ss;
+      ss << '[' << omp_get_thread_num() << ']';
+      for (auto value : slave) ss << ' ' << value;
+      ss << '\n';
+      std::cout << ss.str();
+
+      // Gathers slaves into master
+      master.insert(master.end(),                            //
+                    std::make_move_iterator(slave.begin()),  //
                     std::make_move_iterator(slave.end()));
     }
   }
 
   std::cout << "master std::vector\n";
-  for (auto value : master) {
-    std::cout << value << ' ';
-  }
+  for (auto value : master) std::cout << value << ' ';
   std::cout << '\n' << std::flush;
 }
