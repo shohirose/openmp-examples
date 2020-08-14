2e67fe6fe3c216d04f18fe0f8a6f534a1db1bfcd
Sho Hirose <sho.hirose@gmail.com>
2020-08-14 18:44:54 +0900

    feat: Add gather examples.

---
+++ src/gather.cpp
@@ -0,0 +1,41 @@
+#include <omp.h>
+
+#include <iostream>
+#include <vector>
+
+void gatherStdVector();
+
+int main() { gatherStdVector(); }
+
+void gatherStdVector() {
+  std::vector<int> master;
+  const int n = omp_get_num_procs() * 2;
+  std::cout << "slave std::vectors:\n";
+
+#pragma omp parallel
+  {
+    std::vector<int> slave;
+
+#pragma omp for
+    for (int i = 0; i < n; ++i) {
+      slave.push_back(omp_get_thread_num());
+    }
+
+#pragma omp critical
+    {
+      std::cout << '[' << omp_get_thread_num() << ']';
+      for (auto value : slave) {
+        std::cout << ' ' << value;
+      }
+      std::cout << '\n';
+      master.insert(master.end(), std::make_move_iterator(slave.begin()),
+                    std::make_move_iterator(slave.end()));
+    }
+  }
+
+  std::cout << "master std::vector\n";
+  for (auto value : master) {
+    std::cout << value << ' ';
+  }
+  std::cout << '\n' << std::flush;
+}
