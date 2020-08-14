8da6622ac86860dbb8726e16dc91af8aff055db5
Sho Hirose <sho.hirose@gmail.com>
2020-08-15 03:14:17 +0900

    refactor: Improve how to generate points

--- src/comparison.cpp
+++ src/comparison.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <random>
 
+/// @brief Point in two dimensions
 struct Point {
   double x;
   double y;
@@ -23,27 +24,50 @@ struct Point {
   double rsqr() const noexcept { return x * x + y * y; }
 };
 
-std::vector<Point> generatePoints(size_t numberOfPoints) {
-  std::random_device generator;
-  std::mt19937_64 engine(generator());
-  std::uniform_real_distribution<> dist(0.0, 1.0);
-  std::vector<Point> points(numberOfPoints);
-  const auto n = static_cast<std::int64_t>(numberOfPoints);
+class PointGenerator {
+ public:
+  using result_type = std::vector<Point>;
 
-#pragma omp parallel for
-  for (std::int64_t i = 0; i < n; ++i) {
-    auto &point = points[i];
-    point.x = dist(engine);
-    point.y = dist(engine);
+  PointGenerator(std::size_t numberOfPoints)
+      : numberOfPoints_{numberOfPoints} {}
+
+  /// @brief Generates random points in a region of [0,1]x[0,1] by using STL
+  /// random library.
+  /// @returns A list of points
+  std::vector<Point> operator()() const noexcept {
+    std::vector<Point> points(numberOfPoints_);
+    const auto n = static_cast<std::int64_t>(numberOfPoints_);
+
+#pragma omp parallel
+    {
+      std::random_device rnd;
+      std::mt19937 engine(rnd());
+      std::uniform_real_distribution<> dist(0.0, 1.0);
+
+#pragma omp for
+      for (std::int64_t i = 0; i < n; ++i) {
+        auto &point = points[i];
+        point.x = dist(engine);
+        point.y = dist(engine);
+      }
+    }
+
+    return points;
   }
 
-  return points;
-}
+ private:
+  std::size_t numberOfPoints_;
+};
 
+/// @brief Calculates Pi using Monte Carlo method in serial.
 class SerialPiCalculator {
  public:
+  using result_type = double;
+
+  /// @param[in] points Points in a region of [0,1]x[0,1]
   SerialPiCalculator(const std::vector<Point> &points) : points_{points} {}
 
+  /// @brief Computes Pi
   double operator()() const {
     size_t numberOfPointsInCircle = 0;
     const auto numberOfPoints = points_.size();
@@ -58,10 +82,15 @@ class SerialPiCalculator {
 };
 
 #ifdef _MSC_VER
+/// @brief Calculates Pi using Monte Carlo method by using Microsoft PPL
 struct PplPiCalculator {
  public:
+  using result_type = double;
+
+  /// @param[in] points Points in a region of [0,1]x[0,1]
   PplPiCalculator(const std::vector<Point> &points) : points_{points} {}
 
+  /// @brief Computes Pi
   double operator()() const {
     using concurrency::combinable;
     using concurrency::parallel_for_each;
@@ -79,10 +108,15 @@ struct PplPiCalculator {
 };
 #endif  // _MSC_VER
 
-struct OpenMpPiCalculator {
+/// @brief Calculates Pi using Monte Carlo method by using OpenMP
+struct OmpPiCalculator {
  public:
-  OpenMpPiCalculator(const std::vector<Point> &points) : points_{points} {}
+  using result_type = double;
 
+  /// @param[in] points Points in a region of [0,1]x[0,1]
+  OmpPiCalculator(const std::vector<Point> &points) : points_{points} {}
+
+  /// @brief Computes Pi
   double operator()() const {
     std::int64_t numberOfPointsInCircle = 0;
     const auto numberOfPoints = static_cast<std::int64_t>(points_.size());
@@ -103,37 +137,38 @@ using std::chrono::duration;
 using std::chrono::duration_cast;
 using std::chrono::microseconds;
 
-struct Result {
-  microseconds time;
-  double pi;
-};
-
+/// @brief Measures the execution time of a given functor.
 template <typename F>
-Result measureExecTime(F &&f) {
+std::pair<microseconds, typename F::result_type> measureExecTime(F &&f) {
   using std::chrono::system_clock;
   const auto start = system_clock::now();
-  const auto pi = f();
+  const auto result = f();
   const auto end = system_clock::now();
-  return {duration_cast<microseconds>(end - start), pi};
+  return {duration_cast<microseconds>(end - start), result};
 }
 
 int main() {
-  size_t numberOfPoints = 10'000'000;
+  size_t numberOfPoints = 100'000'000;
+
   std::cout << "Number of points: " << numberOfPoints << std::endl;
-  const auto points = generatePoints(numberOfPoints);
+
+  const auto result0 = measureExecTime(PointGenerator(numberOfPoints));
+  const auto &points = result0.second;
+  std::cout << "Points Generation: time = " << result0.first.count() << " usec"
+            << std::endl;
 
   const auto result1 = measureExecTime(SerialPiCalculator(points));
 #ifdef _MSC_VER
   const auto result2 = measureExecTime(PplPiCalculator(points));
 #endif  // _MSC_VER
-  const auto result3 = measureExecTime(OpenMpPiCalculator(points));
+  const auto result3 = measureExecTime(OmpPiCalculator(points));
 
-  std::cout << "Serial: time = " << std::setw(10) << result1.time.count()
-            << " usec, pi = " << result1.pi
+  std::cout << "Serial: time = " << std::setw(10) << result1.first.count()
+            << " usec, pi = " << result1.second
 #ifdef _MSC_VER
-            << "\nPPL   : time = " << std::setw(10) << result2.time.count()
-            << " usec, pi = " << result2.pi
+            << "\nPPL   : time = " << std::setw(10) << result2.first.count()
+            << " usec, pi = " << result2.second
 #endif  // _MSC_VER
-            << "\nOpenMP: time = " << std::setw(10) << result3.time.count()
-            << " usec, pi = " << result3.pi << std::endl;
+            << "\nOpenMP: time = " << std::setw(10) << result3.first.count()
+            << " usec, pi = " << result3.second << std::endl;
 }
